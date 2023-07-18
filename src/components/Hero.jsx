5546613da8c29039597888f9107aa8fbe5a7a287
dubey748 <dubeyabhishek748@gmail.com>
2023-07-19 02:22:54 +0530

    Developed Navbar, Hero section and skills section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+const Hero = () => {
+  return (
+    <section className="lelative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute insert-0 top-[120px] max-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-82 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}`}>
+            Hi, I'm <span className="text-[#915eff]">ABhishek</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          As a frontend engineer, I thrive <br className="sm:block hidden"/>in tackling complex logical challenges.
+          </p>
+        </div>
+      </div>
+        <ComputersCanvas/>
+        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
